0387ee56f5aa5370537fd9759358a88134d57549
tehcGuy <scraptrashscrap@gmail.com>
2021-10-23 13:08:35 +0200

    Using the instance block

--- src/main/java/com/practice/springbootstarter/controller/topic/MyServerForTopic.java
+++ src/main/java/com/practice/springbootstarter/controller/topic/MyServerForTopic.java
@@ -7,11 +7,16 @@ import java.util.List;
 
 @Service
 public class MyServerForTopic {
-    private List<TopicToStudy> listOfTopics = Arrays.asList(
-            new TopicToStudy(1, "REST API", "very"),
-            new TopicToStudy(2, "JSON", "medium"),
-            new TopicToStudy(3, "servlets", "medium")
-    );
+    private List<TopicToStudy> listOfTopics;
+
+    //instance block is called whenever the class is initialized by Spring Boot
+    { //object block. I have to do the instance block or directly initialize the field
+        listOfTopics = Arrays.asList(
+                new TopicToStudy(1, "REST API", "very"),
+                new TopicToStudy(2, "JSON", "medium"),
+                new TopicToStudy(3, "servlets", "medium")
+        );
+    }
 
     public List<TopicToStudy> getListOfTopics() {
         return listOfTopics;
